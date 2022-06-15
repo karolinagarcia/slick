caf6297b57a58692188c14a3196d950b66c9456e
smsutherland <sagan@farthinghalearms.com>
2022-06-15 10:32:38 -0400

    remove unused scripts

--- slick.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#JOBID=$(sbatch slick1_jobscript.sh | awk '{print $4}')
-#JOBID=$(sbatch -d afterok:$JOBID slick2_jobscript.sh | awk '{print $4}')
-
-sbatch --dependency=singleton --job-name=slick slick1_jobscript.sh
-sbatch --dependency=singleton --job-name=slick slick2_jobscript.sh
