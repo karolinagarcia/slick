0a0c17cd401565cc55702db92f2f3fa155154ecd
smsutherland <sagan@farthinghalearms.com>
2022-06-14 22:22:53 -0400

    improve slick frontend it's less dumb now

--- slick_frontend.sh
+++ slick_frontend.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-INIT_JOB_ID=$(sbatch --parsable slick_init_jobscript.sh)
-
-sbatch --dependency=afterok:$INIT_JOB_ID slick_run_jobscript.sh
-
-echo "slurm jobs queued"
+srun slick_init_jobscript.sh
+srun slick_run_jobscript.sh
+echo "Slick has finished"
