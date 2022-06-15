969a83be69c35c31e502a92767777003a3592284
smsutherland <sagan@farthinghalearms.com>
2022-06-14 22:44:58 -0400

    run init jobscript from its directory

--- slick_frontend.sh
+++ slick_frontend.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-srun slick_init_jobscript.sh $1
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+srun $parent_path/slick_init_jobscript.sh $1
 srun slick_run_jobscript.sh
 echo "Slick has finished"
