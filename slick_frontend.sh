177669ebe6a2bdf3db1ea2005a864277626746c6
smsutherland <sagan@farthinghalearms.com>
2022-06-15 10:28:16 -0400

    combine frontend and init scripts

--- slick_frontend.sh
+++ slick_frontend.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-srun $parent_path/slick_init_jobscript.sh $1
+python slick_init.py
 srun slick_run_jobscript.sh
 echo "Slick has finished"
