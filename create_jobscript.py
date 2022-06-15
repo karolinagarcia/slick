01f18ae247a901bbd63825fc19a902a732cf08f3
smsutherland <sagan@farthinghalearms.com>
2022-06-15 11:44:10 -0400

    write newlines in jobscript

--- create_jobscript.py
+++ create_jobscript.py
@@ -12,9 +12,8 @@ def create_jobscript(param_filename, max_lines, SBATCH_args={}):
     full_SBATCH_args["array"] = f"1-{max_lines}"
 
     with open("slick_run_jobscript.sh", "w") as f:
-        f.write("#!/bin/bash")
-        for arg_name, arg_val in full_SBATCH_args.items():
-            f.write(f"#SBATCH --{arg_name}={arg_val}")
-        f.write(f"""PRAM=$(sed -n "$SLURM_ARRAY_TASK_ID"p {param_filename})""")
-        f.write("""echo $PARAM""")
-        f.write("""python slick_run.py --cloudinfo $PARAM""")
+        f.write("#!/bin/bash\n")
+        f.writelines(f"#SBATCH --{arg_name}={arg_val}\n" for arg_name, arg_val in full_SBATCH_args.items())
+        f.write(f'PRAM=$(sed -n "$SLURM_ARRAY_TASK_ID"p {param_filename})\n')
+        f.write("echo $PARAM\n")
+        f.write("python slick_run.py --cloudinfo $PARAM\n")
