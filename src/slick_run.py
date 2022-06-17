74fc364e82f97fcce4eef4a6a6bb875ce579ed13
smsutherland <sagan@farthinghalearms.com>
2022-06-17 08:55:49 -0400

    re-add header

--- src/slick_run.py
+++ src/slick_run.py
@@ -23,7 +23,7 @@ df_basic = pd.read_csv(glob.glob(f'{config["output_dir"]}/Basic*')[0])
 
 df = pd.DataFrame({'Galaxy_ID':[], 'Cloud_ID':[], 'Mcloud':[], 'Rcloud':[], 'Metallicity':[], 'RadField':[], 'redshift':[], 'H2_lcii':[], 'CO10':[], 'CO21':[], 'CO32':[], 'CO43':[], 'CO54':[], 'CO10_intTB':[], 'CO21_intTB':[], 'CO32_intTB':[], 'CO43_intTB':[], 'CO54_intTB':[], 'CI10':[], 'CI21':[], 'CO65':[], 'CO76':[], 'CO87':[], 'CO98':[], 'CO65_intTB':[], 'CO76_intTB':[], 'CO87_intTB':[], 'CO98_intTB':[], 'OI1':[], 'OI2':[], 'OI3':[], 'fH2':[]})
 
-# df.to_csv(f'{config["output_dir"]}/lim_df_{config["date"]}.csv', index = False)
+df.to_csv(f'{config["output_dir"]}/lim_df.csv', index = False)
 
 creating_table(gal_number, cloud_list, df_basic, config["output_dir"])
 print("Slick run completed successfully")
