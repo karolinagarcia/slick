8e280dbdae177e171762be78a9460a75f4bc9b6e
Karolina Garcia <91089788+karolinagarcia@users.noreply.github.com>
2023-09-27 10:44:14 -0400

    Update slick_init.py

--- src/slick_init.py
+++ src/slick_init.py
@@ -20,6 +20,8 @@ def main():
     
     try:
         mkdir(config["output_dir"])
+    except: pass
+    '''
     except FileExistsError:
         if config["overwrite"]:
             rmtree(config["output_dir"])
@@ -28,7 +30,7 @@ def main():
             print("Output directory already present")
             print("Either specify a new output directory or set the overwrite option.")
             exit(1)
-
+    '''
     # Creates table with basic characteristics for all the clouds
     if not config["skip_basictable"]:
         create_basic_table(config)
@@ -41,8 +43,10 @@ def main():
         create_jobscript(param_filename, max_lines, config_file, config["sbatch"])
 
         # Creates the lim file with the header
-        df = pd.DataFrame({'Galaxy_ID':[], 'Cloud_ID':[], 'Mcloud':[], 'Rcloud':[], 'c_Pressure':[], 'Metallicity':[], 'RadField':[], 'c_DMR':[], 'redshift':[], 'H2_lcii':[], 'CO10':[], 'CO21':[], 'CO32':[], 'CO43':[], 'CO54':[], 'CO10_intTB':[], 'CO21_intTB':[], 'CO32_intTB':[], 'CO43_intTB':[], 'CO54_intTB':[], 'CI10':[], 'CI21':[], 'CO65':[], 'CO76':[], 'CO87':[], 'CO98':[], 'CO65_intTB':[], 'CO76_intTB':[], 'CO87_intTB':[], 'CO98_intTB':[], 'OI1':[], 'OI2':[], 'OI3':[], 'fH2':[], 'fH':[], 'fHp':[], 'gas_temp':[], 'dust_temp':[], 'n_dens':[],'col_dens':[], 'Mol_gas':[], 'CO10_areal_TB':[], 'time':[]})
-        df.to_csv(f'{config["output_dir"]}/lim_df.csv', index = False)
+        try:
+            df = pd.DataFrame({'Galaxy_ID':[], 'Cloud_ID':[], 'Mcloud':[], 'Rcloud':[], 'Pressure':[], 'Metallicity':[], 'RadField':[], 'DMR':[], 'Redshift':[], 'H2_lcii':[], 'CO10':[], 'CO21':[], 'CO32':[], 'CO43':[], 'CO54':[], 'CO10_intTB':[], 'CO21_intTB':[], 'CO32_intTB':[], 'CO43_intTB':[], 'CO54_intTB':[], 'CI10':[], 'CI21':[], 'CO65':[], 'CO76':[], 'CO87':[], 'CO98':[], 'CO65_intTB':[], 'CO76_intTB':[], 'CO87_intTB':[], 'CO98_intTB':[], 'OI1':[], 'OI2':[], 'OI3':[], 'fH2':[], 'fH':[], 'fHp':[], 'fCO':[], 'fCp':[], 'fC':[], 'fO':[], 'fOHx':[], 'gas_temp':[], 'dust_temp':[], 'n_dens':[],'col_dens':[], 'Mol_gas':[], 'CO10_areal_TB':[], 'Time':[], 'NZONES':[], 'Zones_Converged':[]})
+            df.to_csv(f'{config["output_dir"]}/lim_df.csv', index = False, overwrite=False)
+        except: pass
 
 if __name__ == "__main__":
     main()
