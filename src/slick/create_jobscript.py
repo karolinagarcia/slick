a450733b61e75850c45fa0c6039d6c211e4106cb
Sagan Sutherland <sagan@farthinghalearms.com>
2025-01-10 15:25:15 -0500

    Format

--- src/slick/create_jobscript.py
+++ src/slick/create_jobscript.py
@@ -1,5 +1,6 @@
 import pathlib
 
+
 def create_jobscript(param_filename, max_lines, max_cores, param_file, SBATCH_args={}):
     default_SBATCH_args = {
         "nodes": "1",
@@ -8,16 +9,21 @@ def create_jobscript(param_filename, max_lines, max_cores, param_file, SBATCH_ar
         "mem-per-cpu": "8gb",
         "time": "48:00:00",
         "account": "astronomy-dept",
-        "qos": "astronomy-dept-b"#,
-        #"qos": "narayanan-b"#,
-        #"output": "/dev/null"
+        "qos": "astronomy-dept-b",  # ,
+        # "qos": "narayanan-b"#,
+        # "output": "/dev/null"
     }
     # NOTE: this makes no guarantees about the order in which SBATCH args are laid out
     full_SBATCH_args = {**default_SBATCH_args, **SBATCH_args}
-    full_SBATCH_args["array"] = f"1-{max_lines}%{max_cores}" #92 when using 3000 cores
+    full_SBATCH_args["array"] = f"1-{max_lines}%{max_cores}"  # 92 when using 3000 cores
     # the calculation for the highest amount of cores is total we wanna use divided by "cpus-per-task" in slick_run_jobscript file
 
     with open("slick_run_jobscript.sh", "w") as f:
         f.write("#!/bin/bash\n")
-        f.writelines(f"#SBATCH --{arg_name}={arg_val}\n" for arg_name, arg_val in full_SBATCH_args.items())
-        f.write(f"python {pathlib.Path(__file__).parent.resolve()}/slick_run.py --parameters {param_file} --cloudinfofile {param_filename} --cloudinfoline $SLURM_ARRAY_TASK_ID\n")
+        f.writelines(
+            f"#SBATCH --{arg_name}={arg_val}\n"
+            for arg_name, arg_val in full_SBATCH_args.items()
+        )
+        f.write(
+            f"python {pathlib.Path(__file__).parent.resolve()}/slick_run.py --parameters {param_file} --cloudinfofile {param_filename} --cloudinfoline $SLURM_ARRAY_TASK_ID\n"
+        )
--- src/slick/slick_run.py
+++ src/slick/slick_run.py
@@ -7,7 +7,7 @@ from read_config import parse_parameters
 from create_cloudspercore_list import create_cloudspercore_list
 from create_jobscript import create_jobscript
 
-parser = argparse.ArgumentParser(prog='lim_code_simple')
+parser = argparse.ArgumentParser(prog="lim_code_simple")
 parser.add_argument("-p", "--parameters", type=str)
 parser.add_argument("--cloudinfofile", type=str)
 parser.add_argument("--cloudinfoline", type=int)
@@ -16,24 +16,40 @@ args = parser.parse_args()
 
 cloud_info_file = args.cloudinfofile
 with open(cloud_info_file, "r") as f:
-    [*cloud_list] = [int(x) for x in next(x for i, x in enumerate(f) if i == args.cloudinfoline-1).split()]
+    [*cloud_list] = [
+        int(x)
+        for x in next(x for i, x in enumerate(f) if i == args.cloudinfoline - 1).split()
+    ]
 
 config = parse_parameters(args.parameters)
 
-df_basic = pd.read_csv(glob.glob(f'{config["basictable_dir"]}/Basic*')[0])
+df_basic = pd.read_csv(glob.glob(f"{config['basictable_dir']}/Basic*")[0])
 
-df_lim = creating_table(cloud_list, df_basic, config["output_dir"], int(config["n_zones"]))
+df_lim = creating_table(
+    cloud_list, df_basic, config["output_dir"], int(config["n_zones"])
+)
 
 # Running remaining clouds in case any core time was not enough
-set1 = set(df_basic['c_Index'])
-set2 = set(df_lim['Cloud_ID'].unique())
+set1 = set(df_basic["c_Index"])
+set2 = set(df_lim["Cloud_ID"].unique())
 unique_to_list1 = set1 - set2
-if unique_to_list1>0:
-    n_clouds_per_core = config["n_clouds_per_core"]/10
-    param_filename, max_lines = create_cloudspercore_list(config,n_clouds_per_core)
-    create_jobscript(param_filename, max_lines, int(config["max_cores"]), args.parameters, config["sbatch"])
+if unique_to_list1 > 0:
+    n_clouds_per_core = config["n_clouds_per_core"] / 10
+    param_filename, max_lines = create_cloudspercore_list(config, n_clouds_per_core)
+    create_jobscript(
+        param_filename,
+        max_lines,
+        int(config["max_cores"]),
+        args.parameters,
+        config["sbatch"],
+    )
     with open(cloud_info_file, "r") as f:
-        [*cloud_list] = [int(x) for x in next(x for i, x in enumerate(f) if i == args.cloudinfoline-1).split()]
+        [*cloud_list] = [
+            int(x)
+            for x in next(
+                x for i, x in enumerate(f) if i == args.cloudinfoline - 1
+            ).split()
+        ]
     creating_table(cloud_list, df_basic, config["output_dir"], int(config["n_zones"]))
 
 print("Slick run completed successfully")
